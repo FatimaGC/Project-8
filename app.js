3887e7979f67da971851420e51b0f60a349677b7
Fatima Guzman <fguzman1230@gmail.com>
2022-12-11 10:40:50 -0700

    Added displayModal function

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ email, location, phone, dob &noinfo &nat=US`;
 let employees = [];
 const gridContainer = document.querySelector(".grid-container");
 const modalClose = document.querySelector(".modal-close");
+const modalContainer = document.querySelector(".modal-content");
 
 // FETCH API REQUEST
 fetch(urlAPI).then((response) =>
@@ -44,4 +45,36 @@ function displayEmployees(employeeData) {
     `;
   });
   gridContainer.innerHTML = employeeHTML;
+}
+
+function displayModal(index) {
+  let {
+    name,
+    dob,
+    phone,
+    email,
+    location: { city, street, state, postcode },
+    picture,
+  } = employees[index];
+
+  let date = new Date(dob.date);
+
+  const modalHTML = `
+            <img class="profile-image" src="${
+              picture.large
+            }" alt="Profile photo of ${name}" />
+
+            <div class="text-container">
+              <h2 class="name">${name.first} ${name.last}</h2>
+              <p class="email">${email}</p>
+              <p class="address">${city}</p>
+
+              <hr class="modal-content-divider" />
+
+              <p class="phone">${phone}</p>
+              <p class="address">${street}, ${state} ${postcode}</p>
+              <p class="birthday">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+  `;
+  overlay.classList.remove("hidden");
+  modalContainer.innerHTML = modalHTML;
 }
